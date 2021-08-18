fecea60bcde3f0448fe5c7a0ee1a84beb3293d90
Yanick <tyankee15@gmail.com>
2021-08-18 06:17:24 -0300

    Add namespace api/v1 to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :records
-  resources :items
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      resources :records
+      resources :items
+      resources :users
+    end
+  end
 end
