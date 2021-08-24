e395b49346b87c9a9d37e8ddc3c605b456b90b0b
Yanick <tyankee15@gmail.com>
2021-08-24 02:51:53 -0300

    Set up items_controller with necessary methods

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,31 @@
-class ItemsController < ApplicationController
+module Api
+  module V1
+    class ItemsController < ApplicationController
+      before_action :authorized, except: [index, show]
+      before_action :set_item, only: [show, update, destroy]
+
+      def index
+      end
+
+      def show
+      end
+
+      def create
+      end
+
+      def update
+      end
+
+      def destroy
+      end
+
+      private
+
+      def item_params
+      end
+
+      def set_item
+      end
+    end
+  end
 end
