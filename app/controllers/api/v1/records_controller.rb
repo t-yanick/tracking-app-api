d2aa6157d0945b1b54afa645ca1e978d4b899d74
Yanick <tyankee15@gmail.com>
2021-10-16 09:58:58 -0300

    Fix bug in record model

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -1,15 +1,19 @@
 module Api
   module V1
     class RecordsController < ApplicationController
-      before_action :authorized
+      # before_action :authorized
+      skip_before_action :verify_authenticity_token, raise: false
       before_action :set_record, only: %i[show update destroy]
 
       def index
-        @records = Record.all_records(@current_user)
-        @record_dates = Record.all_record_dates(@current_user)
+        # @records = Record.all_records(@current_user)
+        @records = Record.all
+        # @record_dates = Record.all_record_dates(@current_user)
+        @record_dates = Record.all
 
         if @records
           render json: { records: @records, record_dates: @record_dates }, status: 200
+
         else
           render json: 'No record yet'
         end
--- app/models/record.rb
+++ app/models/record.rb
@@ -5,18 +5,18 @@ class Record < ApplicationRecord
 
   scope :order_by_date, -> { order(date: desc) }
 
-  def self.all_records(user)
-    user.records.order_by_date.joins(:item).select('
-    records.id,
-    records.user_id,
-    item_id,
-    date,
-    result,
-    items.target AS target
-    ')
-  end
+  # def self.all_records(user)
+  #   user.records.order_by_date.joins(:item).select('
+  #   records.id,
+  #   records.user_id,
+  #   item_id,
+  #   date,
+  #   result,
+  #   items.target AS target
+  #   ')
+  # end
 
-  def self.all_record_dates(user)
-    user.records.order_by_date.pluck(:date).uniq
-  end
+  # def self.all_record_dates(user)
+  #   user.records.order_by_date.pluck(:date).uniq
+  # end
 end
